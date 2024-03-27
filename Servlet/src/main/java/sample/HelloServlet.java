c2303184e39f7af29a863000ad194048398fc351
wwwalohacampus <h850415@naver.com>
2024-03-27 18:28:54 +0900

    👩‍🏫

---
+++ Servlet/src/main/java/sample/HelloServlet.java
@@ -0,0 +1,49 @@
+package sample;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+// 요청 경로 매핑
+@WebServlet({"/main", "/hello"})
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		response.getWriter().append("/Servlet/main 으로 요청함");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Test/src/main/webapp/main.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<%
+	request
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+	
+
+	<h1>Hello JSP~!</h1>
+	<h3>Yes</h3>
+	<h3>Yes</h3>
+	<h3>No</h3>
+	<h3>No</h3>
+	<h3>No</h3>
+	<h3>No</h3>
+	<h3>No</h3>
+	<h3>No</h3>
+</body>
+</html>
